c939b7722d41602514c43349388753489282de47
Sebastian Meyne <sebastian.meyne@gmail.com>
2021-02-19 20:55:11 +0100

    Added database connectivity (hopefully)

--- src/main/java/de/leuphana/shop/customermicroservice/component/behaviour/CustomerServiceImplementation.java
+++ src/main/java/de/leuphana/shop/customermicroservice/component/behaviour/CustomerServiceImplementation.java
@@ -2,8 +2,16 @@ package de.leuphana.shop.customermicroservice.component.behaviour;
 
 import de.leuphana.shop.customermicroservice.component.structure.Customer;
 import de.leuphana.shop.customermicroservice.component.structure.PostAddress;
+import de.leuphana.shop.customermicroservice.connector.CustomerDatabaseConnector;
 
 public class CustomerServiceImplementation implements CustomerService {
+
+    private static CustomerServiceImplementation customerServiceImplementation;
+
+    public static CustomerServiceImplementation getInstance() {
+        if(customerServiceImplementation == null) customerServiceImplementation = new CustomerServiceImplementation();
+        return customerServiceImplementation;
+    }
 
     @Override
     public Customer createCustomer(String firstname, String lastname, String street, String housenumber, String zipcode, String city) {
@@ -18,6 +26,8 @@ public class CustomerServiceImplementation implements CustomerService {
         postAddress.setCity(city);
 
         customer.setPostAddress(postAddress);
+
+        customer.setId(CustomerDatabaseConnector.getInstance().createCustomer(customer));
 
         return customer;
     }
--- src/main/java/de/leuphana/shop/customermicroservice/connector/CustomerDatabaseConnector.java
+++ src/main/java/de/leuphana/shop/customermicroservice/connector/CustomerDatabaseConnector.java
@@ -11,7 +11,13 @@ import de.leuphana.shop.customermicroservice.connector.entity.CustomerEntity;
 import de.leuphana.shop.customermicroservice.connector.mapper.CustomerMapper;
 
 public class CustomerDatabaseConnector {
+    private static CustomerDatabaseConnector customerDatabaseConnector;
     private EntityManager entityManager;
+
+    public static CustomerDatabaseConnector getInstance() {
+        if(customerDatabaseConnector == null) customerDatabaseConnector = new CustomerDatabaseConnector();
+        return customerDatabaseConnector;
+    }
 
     @PersistenceContext(type = PersistenceContextType.TRANSACTION)
     public void setEntityManager(EntityManager entityManager) {
