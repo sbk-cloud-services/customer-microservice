bb1f806eecd4187b21e4d7ee8b58dfd8669f372d
leoyn <leo@mixify.ga>
2021-03-04 03:50:50 +0100

    Add deployment config and discovery client

--- src/main/java/de/leuphana/shop/customermicroservice/connector/CustomerServiceApplication.java
+++ src/main/java/de/leuphana/shop/customermicroservice/connector/CustomerServiceApplication.java
@@ -3,11 +3,13 @@ package de.leuphana.shop.customermicroservice.connector;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication implements CommandLineRunner {
 
     private static ApplicationContext applicationContext;
